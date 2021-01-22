714df75f49469715ae7e10379f5551b5b7a7ddbb
Emmanuel Peralta <eperalta@webgeoservices.com>
2021-01-22 11:30:35 +0100

    Removes unneeded files.

--- render_toml.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-function render_template() {
-  eval "echo \"$(cat $1)\""
-}
-
-echo "#### Creating ./relay.toml from template ./relay.toml.tmpl"
-render_template relay.toml.tmpl > ./relay.toml
